d87dd9c6ef59175dade8f4c19433f54e2ac72e47
Michelle Duong <m4duong@ucsd.edu>
2020-01-30 11:34:02 -0800

    node.js lab

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
 
-var index = require('./routes/index');
-var hello = require('./routes/hello');
+var index = require("./routes/index");
+var hello = require("./routes/hello");
 // Example route
 // var user = require('./routes/user');
 
@@ -38,9 +38,14 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/hello/:userName', hello.view);
+app.get("/project/:name", project.viewProject);
 // Example route
 // app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+var index = require('./routes/index');
+var project = require('./routes/project');
